af5caf2cc0dd3103a03cc5c45478c8556d879453
Nirmel Murtic <nmurtic@apple.com>
2022-12-14 17:37:34 +0100

    - new challenges added

---
+++ src/main/java/is/symphony/challenge/Challenge13.java
@@ -0,0 +1,124 @@
+package is.symphony.challenge;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import reactor.core.publisher.Flux;
+import reactor.util.function.Tuples;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+public class Challenge13 {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static void main(final String[] args) throws IOException {
+        System.out.println(new Challenge13().getSumOfIndices("src/main/resources/input13.txt"));
+        System.out.println(new Challenge13().getDecoderKey("src/main/resources/input13.txt"));
+    }
+
+    private Stream<List<List>> getGroupedPairs(final Stream<String> lines) {
+        return Flux.fromStream(getPairs(lines))
+                .window(2)
+                .flatMap(Flux::collectList).toStream();
+    }
+
+    private Stream<List> getPairs(final Stream<String> lines) {
+        return lines
+                .filter(line -> !line.isEmpty())
+                .map(line -> {
+                    try {
+                        return objectMapper.readValue(line, List.class);
+                    } catch (JsonProcessingException e) {
+                        throw new IllegalStateException("Invalid input file");
+                    }
+                });
+    }
+
+    private Integer getDecoderKey(final String filePath) throws IOException {
+        final List<List<List<Integer>>> dividers = List.of(List.of(List.of(2)), List.of(List.of(6)));
+
+        try (final Stream<String> lines = (Files.lines(Paths.get(filePath)))) {
+            return Stream.concat(getPairs(lines), dividers.stream())
+                    .sorted(Comparator.comparing((obj) -> obj, (o1, o2) -> {
+                        final Boolean check = checkOrder(o1, o2);
+
+                        if (check == null) {
+                            return 0;
+                        }
+
+                        return check ? -1 : 1;
+                    }))
+                    .reduce(Tuples.of(1, new AtomicInteger(0)), (result, pair) -> {
+                        result.getT2().incrementAndGet();
+
+                        if (dividers.contains(pair)) {
+                            return Tuples.of(result.getT1() * result.getT2().get(), result.getT2());
+                        }
+
+                        return Tuples.of(result.getT1(), result.getT2());
+                    }, (l1, l2) -> l1).getT1();
+        }
+    }
+
+    private Integer getSumOfIndices(final String filePath) throws IOException {
+        try (final Stream<String> lines = (Files.lines(Paths.get(filePath)))) {
+            return getGroupedPairs(lines)
+                    .reduce(Tuples.of(0, new AtomicInteger(0)), (result, pairs) -> {
+                        final Boolean checkResult = checkOrder(pairs.get(0), pairs.get(1));
+
+                        result.getT2().incrementAndGet();
+
+                        if (checkResult == null || checkResult) {
+                            return Tuples.of(result.getT1() + result.getT2().get(), result.getT2());
+                        }
+
+                        return Tuples.of(result.getT1(), result.getT2());
+                    }, (l1, l2) -> l1).getT1();
+        }
+    }
+
+    private Boolean checkOrder(final List list1, final List list2) {
+        for (int i = 0; i < list1.size(); i++) {
+            Object value1 = list1.get(i);
+
+            if (list2.size() <= i) {
+                return false;
+            }
+
+            Object value2 = list2.get(i);
+
+            if (!value1.getClass().equals(value2.getClass())) {
+                if (value1 instanceof Integer) {
+                    value1 = List.of(value1);
+                }
+                else if (value2 instanceof Integer) {
+                    value2 = List.of(value2);
+                }
+            }
+
+            if (!value1.getClass().equals(Integer.class) && !value2.getClass().equals(Integer.class)) {
+                final Boolean result = checkOrder((List) value1, (List) value2);
+
+                if (result != null) {
+                    return result;
+                }
+
+                continue;
+            }
+
+            if ((int) value1 == (int) value2) {
+                continue;
+            }
+
+            return (int) value1 < (int) value2;
+        }
+
+        return list1.size() < list2.size() ? true : null;
+    }
+}
---
+++ src/main/java/is/symphony/challenge/Challenge14.java
@@ -0,0 +1,162 @@
+package is.symphony.challenge;
+
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Challenge14 {
+
+    final static Character SAND_CHARACTER = 'o';
+    final static Character ROCK_CHARACTER = '#';
+    final static Character EMPTY_CHARACTER = '.';
+    final static Character SOURCE_CHARACTER = '+';
+    final static List<Character> END_CHARACTERS = List.of(ROCK_CHARACTER, SAND_CHARACTER);
+
+    public static void main(final String[] args) throws IOException {
+        System.out.println(new Challenge14().execute("src/main/resources/input14.txt", 2));
+    }
+
+    private Integer execute(final String filePath, final Integer floor) throws IOException {
+        try (final Stream<String> lines = (Files.lines(Paths.get(filePath)))) {
+            final List<Tuple2<Integer, Integer>> rocks = lines.flatMap(this::getRockPositions).toList();
+
+            int minY = rocks.stream().mapToInt(Tuple2::getT1).min().orElse(0);
+            int maxX = rocks.stream().mapToInt(Tuple2::getT2).max().orElse(0);
+            int maxY = rocks.stream().mapToInt(Tuple2::getT1).max().orElse(0);
+
+            if (floor != null && floor > 0) {
+                minY -= maxX + floor;
+                maxY += maxX + floor;
+            }
+
+            int[] sourcePosition = new int[] {0, 500 - minY};
+
+            final List<List<Character>> matrix = initializeMatrix(maxX, maxY - minY);
+
+            if (floor != null && floor > 0) {
+                addFloors(matrix, floor);
+            }
+
+            matrix.get(sourcePosition[0]).set(sourcePosition[1], SOURCE_CHARACTER);
+
+            for (final Tuple2<Integer, Integer> rock : rocks) {
+                matrix.get(rock.getT2()).set(rock.getT1() - minY, ROCK_CHARACTER);
+            }
+
+            printMatrix(matrix);
+
+            return IntStream.iterate(0, counter -> counter + 1)
+                    .filter(i -> !addSand(matrix, sourcePosition))
+                    .peek(counter -> printMatrix(matrix))
+                    .findFirst().orElseThrow(() -> new IllegalStateException("Invalid input file!"));
+        }
+    }
+
+    private void addFloors(final List<List<Character>> matrix, final int floor) {
+        final int floorWidth = matrix.get(0).size();
+
+        matrix.addAll(IntStream.range(0, floor).boxed()
+                .map(i -> IntStream.range(0, floorWidth + 1).boxed()
+                        .map(j -> i == floor - 1 ? ROCK_CHARACTER : EMPTY_CHARACTER).collect(Collectors.toList()))
+                .toList());
+    }
+
+    private boolean addSand(final List<List<Character>> matrix, final int[] sourcePosition) {
+        final int[] sandPosition = sourcePosition.clone();
+
+        if (matrix.get(sandPosition[0]).get(sandPosition[1]).equals(SAND_CHARACTER)) {
+            return false;
+        }
+
+        return IntStream.iterate(0, i -> i + 1).boxed()
+                .flatMap(i -> {
+                    try {
+                        if (END_CHARACTERS.contains(matrix.get(sandPosition[0] + 1).get(sandPosition[1]))) {
+                            if (!END_CHARACTERS.contains(matrix.get(sandPosition[0] + 1).get(sandPosition[1] - 1))) {
+                                sandPosition[1] = sandPosition[1] - 1;
+                            }
+                            else if (!END_CHARACTERS.contains(matrix.get(sandPosition[0] + 1).get(sandPosition[1] + 1))) {
+                                sandPosition[1] = sandPosition[1] + 1;
+                            }
+                            else {
+                                matrix.get(sandPosition[0]).set(sandPosition[1], SAND_CHARACTER);
+
+                                return Stream.of(true);
+                            }
+                        }
+
+                        sandPosition[0] = sandPosition[0] + 1;
+                    }
+                    catch (IndexOutOfBoundsException e) {
+                        return Stream.of(false);
+                    }
+
+                    return Stream.empty();
+                }).findAny().orElseThrow(() -> new IllegalStateException("Invalid input file!"));
+    }
+
+    private void printMatrix(final List<List<Character>> matrix) {
+        matrix.forEach(row -> {
+            row.forEach(System.out::print);
+            System.out.println();
+        });
+
+        System.out.println();
+    }
+
+    private List<List<Character>> initializeMatrix(final int sizeX, final int sizeY) {
+        return IntStream.range(0, sizeX + 1).boxed()
+                .map(i -> IntStream.range(0, sizeY + 1)
+                        .mapToObj(j -> EMPTY_CHARACTER).collect(Collectors.toList()))
+                .collect(Collectors.toList());
+    }
+
+    private Stream<Tuple2<Integer, Integer>> getRockPositions(final String line) {
+        final AtomicReference<Tuple2<Integer, Integer>> currentPosition = new AtomicReference<>();
+
+        return Arrays.stream(line.split(" -> "))
+                .map(this::parsePosition)
+                .flatMap(position -> {
+                    final Tuple2<Integer, Integer> previousPosition = currentPosition.get();
+
+                    currentPosition.set(position);
+
+                    if (previousPosition != null) {
+                        if (position.getT1().equals(previousPosition.getT1())) {
+                            return getStream(previousPosition.getT2(), position.getT2())
+                                    .map(pos -> Tuples.of(position.getT1(), pos));
+                        }
+                        else {
+                            return getStream(previousPosition.getT1(), position.getT1())
+                                    .map(pos -> Tuples.of(pos, position.getT2()));
+                        }
+                    }
+
+                    return Stream.empty();
+                });
+    }
+
+    private Stream<Integer> getStream(final int from, final int to) {
+        if (from < to) {
+            return IntStream.range(from, to + 1).boxed();
+        }
+
+        return IntStream.range(to, from + 1).boxed().sorted(Collections.reverseOrder());
+    }
+
+    private Tuple2<Integer, Integer> parsePosition(final String value) {
+        final String[] parts = value.split(",");
+
+        return Tuples.of(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
+    }
+}
